12b5412f8c2d5202ef777dce010fa59434ac41db
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 15:19:10 +0100

    modify app.js

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.get('/square', function (req,res){
+    var result = square(req.query.number);
+    res.status(200).send({result : resut});
+});
+
 module.exports = app;
