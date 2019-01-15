709b568ad84192c2663d8454f02b2d99c315cd6b
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 16:02:08 +0100

    modify app.test.js

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./app');
 
 test('GET/?number= positiveNumber shoud return 200', async () =>{
-    const response = await request(app).get('/square?number='+ 4);
+    const response = await request(app).get('/square?number='+ 's');
     expect(response.statusCode).toBe(200); 
     expect(response.body.result).toBe(2);
 });
