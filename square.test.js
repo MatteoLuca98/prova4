67964c3c9787b9aa071c0911f126a1626327f608
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 15:49:18 +0100

    moify square.test.js

--- square.test.js
+++ square.test.js
@@ -1,13 +1,13 @@
 const square = require('./square');
 
 test('square(1) should be return 1', () => {
-    expect(square(1).toBe(1));
+    expect(square(1)).toBe(1);
 });
 
 test('square(-8) should be return error', () => {
-    expect(square(-8).toBe("error number is negative"));
+    expect(square(-8)).toBe("error, number is negative");
 });
 
 test('square("a") should be return -1', () => {
-    expect(square("a").toBe(-1));
+    expect(square("a")).toBe(-1);
 });
